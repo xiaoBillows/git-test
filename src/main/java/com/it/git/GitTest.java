5236594074428a848dcc3b67576194be5e72486f
xiaoHe <617652920@qq.com>
2023-03-13 16:55:20 +0800

    second commit

--- src/main/java/com/it/git/GitTest.java
+++ src/main/java/com/it/git/GitTest.java
@@ -10,5 +10,6 @@ package com.it.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
     }
 }
