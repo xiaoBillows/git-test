0d7d588306f9d534954c92188ab427c7ac351dc3
xiaoBillows <127718123+xiaoBillows@users.noreply.github.com>
2023-03-13 18:14:04 +0800

    pull commit

--- src/main/java/com/it/git/GitTest.java
+++ src/main/java/com/it/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("Hello git4  master test!");
         System.out.println("Hello git5  hot-fix test!");
         System.out.println("Hello git5  git_hub push test!");
+        System.out.println("Hello git5  git_hub pull test!");
     }
 }
