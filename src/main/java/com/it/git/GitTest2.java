a0f6becf618486ed34d2092330c83b05f584a393
xiaoHe <617652920@qq.com>
2023-03-13 16:52:50 +0800

    first commit

---
+++ src/main/java/com/it/git/GitTest2.java
@@ -0,0 +1,14 @@
+package com.it.git;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: XiaoHe
+ * @Date: 2023/03/13   16:42
+ * @Description:
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
