8749d143668f5cc8e96a92ec4b4bf96eaa2cb48d
xiaoHe <617652920@qq.com>
2023-03-15 18:52:22 +0800

    gitlab first commit

---
+++ gitlab_test1/src/main/java/com/it/GitlabTest1.java
@@ -0,0 +1,14 @@
+package com.it;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: XiaoHe
+ * @Date: 2023/03/15   18:51
+ * @Description:
+ */
+public class GitlabTest1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
